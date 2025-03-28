acef3e5b55eefc90b138f5661aa2cafa645ba1c9
Joaquin Quintanilla Ramirez <joaquin.quintanilla@alumnos.uv.cl>
2025-03-28 18:21:48 -0300

    endpoint upload + requeriments

--- app/api/endpoints.py
+++ app/api/endpoints.py
@@ -1,8 +1,15 @@
 # -*- coding: utf-8 -*-
-from fastapi import APIRouter
+from fastapi import APIRouter, UploadFile, File
 
 router = APIRouter()
 
 @router.get("/test")
 def test_endpoint():
     return {"message": "Endpoint de prueba exitoso"}
+
+@router.post("/upload/")
+async def upload_file(file: UploadFile = File(...)):
+    return {
+        "filename": file.filename,
+        "content_type": file.content_type
+    }
